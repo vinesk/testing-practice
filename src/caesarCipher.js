67c01b5a20647e73cfd9f59ed42ca78cdc195061
vinesk <vinesk.dev@gmail.com>
2024-04-11 13:15:34 +0200

    Update ceaesarCipher.js for greater readability

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,16 +1,24 @@
-function caesarCipher(string, shift) {
-  return string
+function caesarCipher(str, shift) {
+  const shiftAmount = shift % 26
+
+  return str
     .split('')
     .map((char) => {
-      if (!char.match(/[a-z]/i)) return char
-      const code = char.charCodeAt()
-      let shiftCode = shift % 26
-      if (code >= 65 && code <= 90) {
-        return String.fromCharCode(((code - 65 + shiftCode) % 26) + 65)
+      if (char.match(/[a-z]/i)) {
+        const charCode = char.charCodeAt(0)
+        let shiftedCharCode = charCode + shiftAmount
+
+        if (charCode >= 65 && charCode <= 90) {
+          if (shiftedCharCode > 90) shiftedCharCode -= 26
+          if (shiftedCharCode < 65) shiftedCharCode += 26
+        } else if (charCode >= 97 && charCode <= 122) {
+          if (shiftedCharCode > 122) shiftedCharCode -= 26
+          if (shiftedCharCode < 97) shiftedCharCode += 26
+        }
+
+        return String.fromCharCode(shiftedCharCode)
       }
-      if (code >= 97 && code <= 122) {
-        return String.fromCharCode(((code - 97 + shiftCode) % 26) + 97)
-      }
+      return char
     })
     .join('')
 }
